a7db25260deda9ad939981448e354f268cf5136e
Erik Bolch <ebolch@contractor.usgs.gov>
2023-12-10 10:34:06 -0600

    Built site for gh-pages

--- site_libs/quarto-nav/quarto-nav.js
+++ site_libs/quarto-nav/quarto-nav.js
@@ -8,47 +8,6 @@ const headroomChanged = new CustomEvent("quarto-hrChanged", {
 window.document.addEventListener("DOMContentLoaded", function () {
   let init = false;
 
-  // Manage the back to top button, if one is present.
-  let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  const scrollDownBuffer = 5;
-  const scrollUpBuffer = 35;
-  const btn = document.getElementById("quarto-back-to-top");
-  const hideBackToTop = () => {
-    btn.style.display = "none";
-  };
-  const showBackToTop = () => {
-    btn.style.display = "inline-block";
-  };
-  if (btn) {
-    window.document.addEventListener(
-      "scroll",
-      function () {
-        const currentScrollTop =
-          window.pageYOffset || document.documentElement.scrollTop;
-
-        // Shows and hides the button 'intelligently' as the user scrolls
-        if (currentScrollTop - scrollDownBuffer > lastScrollTop) {
-          hideBackToTop();
-          lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
-        } else if (currentScrollTop < lastScrollTop - scrollUpBuffer) {
-          showBackToTop();
-          lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
-        }
-
-        // Show the button at the bottom, hides it at the top
-        if (currentScrollTop <= 0) {
-          hideBackToTop();
-        } else if (
-          window.innerHeight + currentScrollTop >=
-          document.body.offsetHeight
-        ) {
-          showBackToTop();
-        }
-      },
-      false
-    );
-  }
-
   function throttle(func, wait) {
     var timeout;
     return function () {
@@ -129,7 +88,6 @@ window.document.addEventListener("DOMContentLoaded", function () {
     let linkStyle = window.document.querySelector("#quarto-target-style");
     if (!linkStyle) {
       linkStyle = window.document.createElement("style");
-      linkStyle.setAttribute("id", "quarto-target-style");
       window.document.head.appendChild(linkStyle);
     }
     while (linkStyle.firstChild) {
@@ -190,23 +148,11 @@ window.document.addEventListener("DOMContentLoaded", function () {
     };
   }
 
-  window.addEventListener(
-    "hashchange",
-    function (e) {
-      if (
-        getComputedStyle(document.documentElement).scrollBehavior !== "smooth"
-      ) {
-        window.scrollTo(0, window.pageYOffset - headerOffset());
-      }
-    },
-    false
-  );
-
   // Observe size changed for the header
   const headerEl = window.document.querySelector("header.fixed-top");
   if (headerEl && window.ResizeObserver) {
     const observer = new window.ResizeObserver(
-      updateDocumentOffsetWithoutAnimation
+      throttle(updateDocumentOffsetWithoutAnimation, 50)
     );
     observer.observe(headerEl, {
       attributes: true,
@@ -218,16 +164,14 @@ window.document.addEventListener("DOMContentLoaded", function () {
       "resize",
       throttle(updateDocumentOffsetWithoutAnimation, 50)
     );
+    setTimeout(updateDocumentOffsetWithoutAnimation, 500);
   }
-  setTimeout(updateDocumentOffsetWithoutAnimation, 250);
 
   // fixup index.html links if we aren't on the filesystem
   if (window.location.protocol !== "file:") {
     const links = window.document.querySelectorAll("a");
     for (let i = 0; i < links.length; i++) {
-      if (links[i].href) {
-        links[i].href = links[i].href.replace(/\/index\.html/, "/");
-      }
+      links[i].href = links[i].href.replace(/\/index\.html/, "/");
     }
 
     // Fixup any sharing links that require urls
